5c2e451c258e919fe8bb9d2bc23e6e308deac4bc
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-11-10 19:06:52 -0700

    Configure dynamic styling of sort by menu options

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -24,7 +24,14 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map((sortByOption) => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return (
+        <li
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   }
 
