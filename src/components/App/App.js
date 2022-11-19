361da260c4c8633925d6649ef4dd21f5b4a9e565
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-11-18 21:34:36 -0700

    Import Yelp to App, nix hard-coded business info

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,7 @@ import React from "react";
 import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-
-const business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-const businesses = [business, business, business, business, business, business];
+import Yelp from "../../util/Yelp";
 
 class App extends React.Component {
   searchYelp(term, location, sortBy) {
@@ -34,5 +21,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// Note for future refactoring: I followed the project guidelines, but I find this approach with the businesses array clunky and inelegant. I'll identify a better approach when I start integrating with the Yelp API.
